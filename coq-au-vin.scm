34af14f98497aa90e337773a056fb9927be332b3
mgushee <matt@gushee.net>
2013-03-03 16:16:55 -0700

    Created project.

---
+++ coq-au-vin.scm
@@ -0,0 +1,18 @@
+;;; coq-au-vin.scm -- A simple blogging platform based on Chicken Scheme.
+;;;   Copyright © 2013 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD license. See the accompanying LICENSE file for details.
+
+(module coq-au-vin
+
+
+) ; END MODULE
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
