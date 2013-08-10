5bdd6a53a5f3ba7f95462849c9781c372281f3cb
mgushee <matt@gushee.net>
2013-08-09 22:09:45 -0600

    Added source file for standalone version.

---
+++ cav-standalone.scm
@@ -0,0 +1,15 @@
+;;; cav-standalone.scm -- Source code for standalone executable version.
+;;;
+;;;   Copyright © 2013 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD license. See the accompanying LICENSE file for details.
+
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
