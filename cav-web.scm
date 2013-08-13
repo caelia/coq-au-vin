c3a5e7f68149b2fe94968352420efb76b7d21cce
mgushee <matt@gushee.net>
2013-08-12 19:29:11 -0600

    Added abstract cav-web module and its implementations, cav-web-spiffy and cav-web-fcgi.

---
+++ cav-web.scm
@@ -0,0 +1,24 @@
+;;; {DESCRIPTION}
+;;;   Copyright © 2013 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD license. See the accompanying LICENSE file for details.
+
+(module cav-web
+        *
+        (import scheme chicken)
+
+        (use uri-match)
+
+(define run (make-parameter #f))
+
+) ; END MODULE
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
+; vim:et:ai:ts=2 sw=2
