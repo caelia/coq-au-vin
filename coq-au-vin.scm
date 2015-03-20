cb3b09ee318a9c8ae28d9dc0745a5b4cf337a606
Matt Gushee <matt@gushee.net>
2015-03-20 17:22:28 -0600

    Corrected syntax error.

--- coq-au-vin.scm
+++ coq-au-vin.scm
@@ -386,7 +386,7 @@
   (or (eqv? action 'view-content)
       (and session-key
            (let ((session
-                   (hash-table-ref/default session-store session-key #f))))
+                   (hash-table-ref/default session-store session-key #f)))
              (and (session-valid? session-key session ip)
                   (let ((role (string->symbol (session-role session))))
                     (cond
